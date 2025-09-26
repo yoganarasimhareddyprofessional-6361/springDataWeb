9e5e0379319130970c0d44ad22acec13cb9e91f0
P YOGANARASIMHA REDDY <yoganarasimhareddy.professional@gmail.com>
2025-09-27 03:22:07 +0530

    initial commit --project creation

---
+++ src/main/java/com/yog/test/springdataweb/SpringDataWebApplication.java
@@ -0,0 +1,13 @@
+package com.yog.test.springdataweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yog/test/springdataweb/SpringDataWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yog.test.springdataweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
